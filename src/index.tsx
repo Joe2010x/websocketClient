df875ffeb84509e6fa2d37e78b14c96abdb9f6db
Joe Sun <dps_swanuk@hotmail.com>
2023-07-31 18:29:41 +0200

    completed demo

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorPage } from './pages/ErrorPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={ErrorPage()}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
