dd5acbc0e78d1769adf6dd982a2c613a41968ff4
Dongping Joe Sun <64711803+Joe2010x@users.noreply.github.com>
2023-09-13 21:03:56 +0200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, fireEvent, RenderResult, act} from '@testing-library/react';
 import App from './App';
 import { get } from 'https';
 
-test('clicking the second child of div with id "colorBar" changes the background color of the first child of div with className "inputBox" to yellow', () => {
+test('clicking the second color of div with id "colorBar" changes the background color of the div "inputBox" to "#ffcc70"', () => {
     
 const { getByText, getByTestId }: RenderResult = render(<App />);
 const colorBar = getByTestId('colorBar');
