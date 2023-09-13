e91f6328ec626fcc72eb6779e802c7115c3cfb8c
Joe Sun <dps_swanuk@hotmail.com>
2023-09-13 20:56:08 +0200

    added test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, RenderResult, act} from '@testing-library/react';
 import App from './App';
+import { get } from 'https';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('clicking the second child of div with id "colorBar" changes the background color of the first child of div with className "inputBox" to yellow', () => {
+    
+const { getByText, getByTestId }: RenderResult = render(<App />);
+const colorBar = getByTestId('colorBar');
+const secondColor: HTMLElement = colorBar.children[1] as HTMLElement;
+const inputBox = getByTestId('inputDiv');
+
+act(() => {
+  fireEvent.click(secondColor);
 });
+expect(inputBox).toHaveStyle({ backgroundColor: "#ffcc70" });
+});
+
